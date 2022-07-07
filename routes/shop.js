dad384dc7e5043e1b1093ee2a5f38cc010042281
firmansyah <faruq.uts@gmail.com>
2022-07-07 10:51:55 +0700

    add - delete product in cart

--- routes/shop.js
+++ routes/shop.js
@@ -9,6 +9,7 @@ routes.get('/products', shopController.getProducts);
 routes.get('/products/:productId', shopController.getProduct);
 routes.get('/cart', shopController.getCart);
 routes.post('/cart', shopController.postCart);
+routes.post('/cart-delete-item', shopController.postCartDeleteProduct);
 routes.get('/orders', shopController.getOrders);
 routes.get('/checkout', shopController.getCheckout);
 
