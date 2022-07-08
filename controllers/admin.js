611369ecc9b506c125f7aecd35f3c7bfa713a3ea
firmansyah <faruq.uts@gmail.com>
2022-07-08 11:00:09 +0700

    update - use association function to get user's product

--- controllers/admin.js
+++ controllers/admin.js
@@ -27,17 +27,20 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId).then((product) => {
-    if (!product) {
-      return res.redirect('/');
-    }
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      editing: editMode,
-      product: product,
-    });
-  }).catch((err) => console.error(err));
+  req.user.getProducts({where: {id: prodId}})
+  // Product.findByPk(prodId)
+      .then((products) => {
+        const product = products[0];
+        if (!product) {
+          return res.redirect('/');
+        }
+        res.render('admin/edit-product', {
+          pageTitle: 'Edit Product',
+          path: '/admin/edit-product',
+          editing: editMode,
+          product: product,
+        });
+      }).catch((err) => console.error(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -68,7 +71,8 @@ exports.postDeleteProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user.getProducts()
+  // Product.findAll()
       .then((products) => {
         res.render('admin/products', {
           prods: products,
