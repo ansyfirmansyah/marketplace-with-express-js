fe4331b90b18ad1cf2dc43a7cd030d10020c7c31
firmansyah <faruq.uts@gmail.com>
2022-07-20 17:41:36 +0700

    add - middleware to handle authentication

--- routes/admin.js
+++ routes/admin.js
@@ -3,12 +3,13 @@ const express = require('express');
 // eslint-disable-next-line new-cap
 const routes = express.Router();
 const adminController = require('../controllers/admin');
+const isAuth = require('../middleware/is-auth');
 
-routes.get('/add-product', adminController.getAddProduct);
-routes.post('/add-product', adminController.postAddProduct);
-routes.get('/edit-product/:productId', adminController.getEditProduct);
-routes.post('/edit-product', adminController.postEditProduct);
-routes.post('/delete-product', adminController.postDeleteProduct);
-routes.get('/products', adminController.getProducts);
+routes.get('/add-product', isAuth, adminController.getAddProduct);
+routes.post('/add-product', isAuth, adminController.postAddProduct);
+routes.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
+routes.post('/edit-product', isAuth, adminController.postEditProduct);
+routes.post('/delete-product', isAuth, adminController.postDeleteProduct);
+routes.get('/products', isAuth, adminController.getProducts);
 
 module.exports = routes;
--- routes/shop.js
+++ routes/shop.js
@@ -3,15 +3,16 @@ const express = require('express');
 // eslint-disable-next-line new-cap
 const routes = express.Router();
 const shopController = require('../controllers/shop');
+const isAuth = require('../middleware/is-auth');
 
 routes.get('/', shopController.getIndex);
 routes.get('/products', shopController.getProducts);
 routes.get('/products/:productId', shopController.getProduct);
-routes.get('/cart', shopController.getCart);
-routes.post('/cart', shopController.postCart);
-routes.post('/cart-delete-item', shopController.postCartDeleteProduct);
-routes.post('/create-order', shopController.postOrder);
-routes.get('/orders', shopController.getOrders);
+routes.get('/cart', isAuth, shopController.getCart);
+routes.post('/cart', isAuth, shopController.postCart);
+routes.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
+routes.post('/create-order', isAuth, shopController.postOrder);
+routes.get('/orders', isAuth, shopController.getOrders);
 // routes.get('/checkout', shopController.getCheckout);
 
 module.exports = routes;
